c902d74354f4243dd90f77aaeddbc422f7a9b4ce
kaicheng928 <kaifresh28@gmail.com>
2024-02-25 14:35:15 +0800

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '52727adc-c314-4dae-a607-55a2d25e872c';
+export const MODEL_CLOSE_SEC = 2.5;
